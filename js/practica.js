656b6e95c9d11f0237140c41a99f104cdd134bf9
Deathvks <dylanjesussuarez@gmail.com>
2024-01-29 12:49:26 +0000

    A simple page of notices (for now)

---
+++ js/practica.js
@@ -0,0 +1,55 @@
+function initialize() {
+    const DARK_MODE = document.getElementById('dark-mode');
+    DARK_MODE.addEventListener('click', showDarkMode);
+
+    // Al cargar la página, verifica si el modo oscuro está activado en el local storage
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.style.backgroundColor = 'black';
+        document.querySelector('.topic').style.color = "white";
+        document.getElementById('after-header').style.backgroundColor = "rgb(23, 23, 24)";
+        document.getElementById('one').style.backgroundColor = "black";
+        document.getElementById('two').style.backgroundColor = "black";
+        document.getElementById('title').style.color = "white";
+        document.getElementById('title-two').style.color = "white";
+        DARK_MODE.innerText = 'Tema Claro';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.querySelector('.topic').style.color = "black";
+        document.getElementById('after-header').style.backgroundColor = "rgb(230, 233, 235)";
+        document.getElementById('one').style.backgroundColor = "white";
+        document.getElementById('two').style.backgroundColor = "white";
+        document.getElementById('title').style.color = "black";
+        document.getElementById('title-two').style.color = "black";
+        DARK_MODE.innerText = 'Tema Oscuro';
+    }
+}
+
+function showDarkMode() {
+
+    const DARK_MODE = document.getElementById('dark-mode');
+
+    if (document.body.style.backgroundColor === 'white') {
+        document.body.style.backgroundColor = 'black';
+        document.querySelector('.topic').style.color = "white";
+        document.getElementById('after-header').style.backgroundColor = "rgb(23, 23, 24)";
+        document.getElementById('one').style.backgroundColor = "black";
+        document.getElementById('two').style.backgroundColor = "black";
+        document.getElementById('title').style.color = "white";
+        document.getElementById('title-two').style.color = "white";
+        DARK_MODE.innerText = 'Tema Claro';
+
+        localStorage.setItem('theme', 'dark'); // Guarda el tema en modo oscuro
+    } else {
+        document.body.style.backgroundColor = 'white';
+        DARK_MODE.innerText = 'Tema Oscuro';
+        document.querySelector('.topic').style.color = "black";
+        document.getElementById('after-header').style.backgroundColor = "rgb(230, 233, 235)";
+        document.getElementById('one').style.backgroundColor = "white";
+        document.getElementById('two').style.backgroundColor = "white";
+        document.getElementById('title').style.color = "black";
+        document.getElementById('title-two').style.color = "black";
+        localStorage.setItem('theme', 'light'); // Guarda el tema en modo claro
+    }
+}
+
+initialize();
